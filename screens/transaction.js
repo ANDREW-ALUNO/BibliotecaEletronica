86c089036aefb348164954fb98b51570677ba679
ANDREW-ALUNO <andrew.silva@alu.apac.org.br>
2023-06-23 16:14:14 -0300

    MAIS MELHORIAS

--- screens/transaction.js
+++ screens/transaction.js
@@ -17,8 +17,8 @@ export default class Transaction extends React.Component {
       scanned: false,
       bookId: "",
       studentId: "",
-      bookName:"",
-      studentName:""
+      bookName: "",
+      studentName: ""
     }
   }
   getCameraPermissions = async (domState) => {
@@ -48,45 +48,72 @@ export default class Transaction extends React.Component {
     }
 
   }
-  handleTransaction = async() => {
-    var { bookId,studentId } = this.state
+  handleTransaction = async () => {
+    var { bookId, studentId } = this.state
     await this.getBookDetails(bookId)
     await this.getStudentDetails(studentId)
-    Db.collection('books')
-      .doc(bookId)
-      .get().then(doc => {
-      var book = doc.data()
-      console.log(book)
-      var{bookName ,studentName}=this.state
-      if(book.is_book_available) {
-        this.initiateBookIssue(bookId,studentId,bookName,studentName)
-        if (Platform.OS=='android') {
-          ToastAndroid.show('LIVRO RETIRADO COM SUCESSO :)',ToastAndroid.SHORT)
+    var { bookName, studentName } = this.state
+    var transactionType = this.checkBookAvailability(bookId)
+    if (!transactionType) {
+      if (Platform.OS == 'android') {
+        ToastAndroid.show('LIVRO NÃO ENCONTRADO :(', ToastAndroid.SHORT)
+      } else {
+        Alert.alert('LIVRO NÃO ENCONTRADO :(')
+
+      }
+    }
+    else if (transactionType == 'issue') {
+      var isEligible = this.checkStudentEligibilityForBookIssue(studentId)
+      if (isEligible) {
+        this.initiateBookIssue(bookId, studentId, bookName, studentName)
+        if (Platform.OS == 'android') {
+          ToastAndroid.show('LIVRO RETIRADO COM SUCESSO :)', ToastAndroid.SHORT)
         } else {
-        Alert.alert('LIVRO RETIRADO COM SUCESSO :)')
-          
+          Alert.alert('LIVRO RETIRADO COM SUCESSO :)')
+
         }
       }
-      else{
-        this.initiateBookReturn(bookId,studentId,bookName,studentName)
-        if (Platform.OS=='android') {
-          ToastAndroid.show('LIVRO DEVOLVIDO',ToastAndroid.SHORT)
+
+    }
+
+    else if (transactionType == "return") {
+      var isEligible = this.checkStudentEligibilityForBookReturn(bookId, studentId)
+      if (isEligible) {
+
+        this.initiateBookReturn(bookId, studentId, bookName, studentName)
+        if (Platform.OS == 'android') {
+          ToastAndroid.show('LIVRO DEVOLVIDO', ToastAndroid.SHORT)
         } else {
-        Alert.alert('LIVRO DEVOLVIDO')
-          
+          Alert.alert('LIVRO DEVOLVIDO')
+
         }
       }
-      })
+    }
+
   }
-  getBookDetails = (bookId)=>{
+  getBookDetails = (bookId) => {
     bookId = bookId.toLowerCase().trim()
     Db.collection('books')
-      .where('book_id','==',bookId)
-      .get().then((snapshot)=>{
-        snapshot.docs.map((doc)=>{
-          this.setState({bookName:doc.data().book_name})
+      .where('book_id', '==', bookId)
+      .get().then((snapshot) => {
+        snapshot.docs.map((doc) => {
+          this.setState({ bookName: doc.data().book_name })
         })
       })
+  }
+  checkBookAvailability = async (bookId) => {
+    var bookRef = await Db.collection('books')
+      .where("book_id", '==', bookId)
+      .get()
+    var transactionType = ''
+    if (bookRef.docs.length == 0) {
+      transactionType = false
+    } else {
+      bookRef.docs.map((doc) => {
+        transactionType = doc.data().is_book_available ? 'issue' : 'return'
+      })
+    }
+    return transactionType
   }
   getStudentDetails = studentId => {
     studentId = studentId.trim().toLowerCase();
@@ -100,6 +127,47 @@ export default class Transaction extends React.Component {
           });
         });
       });
+  };
+  checkStudentEligibilityForBookIssue = async studentId => {
+    const studentRef = await Db
+      .collection("students")
+      .where("student_id", "==", studentId)
+      .get();
+
+    var isStudentEligible = "";
+    if (studentRef.docs.length == 0) {
+      this.setState({
+        bookId: "",
+        studentId: ""
+      });
+      isStudentEligible = false;
+      if (Platform.OS == 'android') {
+        ToastAndroid.show('ID DO ALUNO NÃO ENCONTRADO', ToastAndroid.SHORT)
+      } else {
+        Alert.alert('ID DO ALUNO NÃO ENCONTRADO')
+
+      }
+    } else {
+      studentRef.docs.map(doc => {
+        if (doc.data().number_of_books_issued < 2) {
+          isStudentEligible = true;
+        } else {
+          isStudentEligible = false;
+          if (Platform.OS == 'android') {
+            ToastAndroid.show('O ALUNO JA RETIROU DOIS LIVROS!', ToastAndroid.SHORT)
+          } else {
+            Alert.alert('O ALUNO JA RETIROU DOIS LIVROS!')
+
+          }
+          this.setState({
+            bookId: "",
+            studentId: ""
+          });
+        }
+      });
+    }
+
+    return isStudentEligible;
   };
   initiateBookIssue = async (bookId, studentId, bookName, studentName) => {
     //adicionar uma transação
@@ -129,6 +197,33 @@ export default class Transaction extends React.Component {
       bookId: "",
       studentId: ""
     });
+  };
+  checkStudentEligibilityForBookReturn = async (bookId, studentId) => {
+    const transactionRef = await Db
+      .collection("transactions")
+      .where("book_id", "==", bookId)
+      .limit(1)
+      .get();
+    var isStudentEligible = "";
+    transactionRef.docs.map(doc => {
+      var lastBookTransaction = doc.data();
+      if (lastBookTransaction.student_id === studentId) {
+        isStudentEligible = true;
+      } else {
+        isStudentEligible = false;
+        if (Platform.OS == 'android') {
+          ToastAndroid.show('O LIVRO NÃO FOI RETIRADO POR ESTE ALUNO', ToastAndroid.SHORT)
+        } else {
+          Alert.alert('O LIVRO NÃO FOI RETIRADO POR ESTE ALUNO')
+
+        }
+        this.setState({
+          bookId: "",
+          studentId: ""
+        });
+      }
+    });
+    return isStudentEligible;
   };
 
   initiateBookReturn = async (bookId, studentId, bookName, studentName) => {
@@ -181,7 +276,7 @@ export default class Transaction extends React.Component {
                 placeholder={"ID do Livro"}
                 placeholderTextColor={"#FFFFFF"}
                 value={bookId}
-                onChangeText={(text)=>{this.setState({bookId:text.toLowerCase().trim()})}}
+                onChangeText={(text) => { this.setState({ bookId: text.toLowerCase().trim() }) }}
               />
               <TouchableOpacity
                 style={styles.scanbutton}
@@ -196,7 +291,7 @@ export default class Transaction extends React.Component {
                 placeholder={"ID do Estudante"}
                 placeholderTextColor={"#FFFFFF"}
                 value={studentId}
-                onChangeText={(text)=>{this.setState({studentId:text.toLowerCase().trim()})}}
+                onChangeText={(text) => { this.setState({ studentId: text.toLowerCase().trim() }) }}
               />
               <TouchableOpacity
                 style={styles.scanbutton}
